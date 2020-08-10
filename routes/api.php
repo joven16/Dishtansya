d896a6869d98df763a3d54808038cf546e8ec462
Joven <jovenlusterio16@gmail.com>
2020-08-10 16:54:09 +0800

    Dishtansya api

--- routes/api.php
+++ routes/api.php
@@ -13,20 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-// // Route::get('country', 'Country\CountryController@country');
-// // Route::get('country/{id}', 'Country\CountryController@countryByID');
-// // Route::post('country', 'Country\CountryController@countrySave');
-// // Route::put('country/{id}', 'Country\CountryController@countryUpdate');
-// // Route::delete('country/{id}', 'Country\CountryController@countryDelete');
-// Route::group(['middleware' => 'auth:api'], function(){
-//     Route::apiResource('country', 'Country\Country');
-// });
-
-
 Route::post('login', 'WebApi@login');
 Route::post('register', 'WebApi@register');
 Route::group(['middleware' => 'auth:api'], function(){
